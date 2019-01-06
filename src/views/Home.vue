e6d51df228b57e19ae195302fc97ad1119e85e71
Cesar Martinez <cesar.martinez@globalgaming.com>
2019-01-06 17:56:58 +0100

    Vuetify setup. Basic routing. Dashboard container

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,11 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <HelloWorld />
 </template>
 
 <script>
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
   components: {
     HelloWorld
   }
