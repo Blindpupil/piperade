270cc57fc68581e990c6fc89b4261406c299c877
Cesar Martinez <cesar.martinez@globalgaming.com>
2019-01-06 16:38:36 +0100

    Firebase and linter

---
+++ src/firebase/index.js
@@ -0,0 +1,18 @@
+import * as firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+import config from '@/firebase/config'
+
+// Shtaart
+firebase.initializeApp(config)
+
+const firestore = firebase.firestore()
+const settings = { timestampsInSnapshots: true }
+firestore.settings(settings)
+
+// Auth function
+export const auth = firebase.auth()
+
+// Firestore
+const db = firebase.firestore()
+export const usersRef = db.collection('users')
